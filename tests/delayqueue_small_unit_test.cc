5a6fdc17f9957b827e72f8988c66b5c411a59f3b
Xi Cheng <cx19871012@gmail.com>
2020-07-26 22:03:11 +0000

    [test] Incrementally adding test, add a case that task with higher delay time added first, but verify that it gets executed as the last one

--- tests/delayqueue_small_unit_test.cc
+++ tests/delayqueue_small_unit_test.cc
@@ -7,16 +7,21 @@
 
 #include "gtest/gtest.h"
 #include "src/delay_queue.h"
+#include "src/threadsafe_queue.h"
 
 class DelayQueueSmallUnitTest : public ::testing::Test {
  protected:
   DelayQueue delay_queue_;
-};
+  // Used to store results of int during testing
+  ThreadsafeQueue<int> int_res_queue_;
 
-int add(int a, int b) {
-  std::cout << a << " " << b << std::endl; 
-  return a + b;
-}
+ public:
+  int add(int a, int b) {
+    int res(a + b);
+    int_res_queue_.Push(res);
+    return res;
+  }
+};
 
 // Define several tasks with different delay time and makes sure that they all
 // execute with expected returns. In this specific case, we insert tasks 
@@ -27,11 +32,47 @@ TEST_F(DelayQueueSmallUnitTest, AddTaskInSequence) {
   // Insert tasks with different delay
   for (int i = 0; i < num_tasks; i++) {
     task_future.emplace_back(
-        delay_queue_.AddTask(i * 200, std::bind(add, i, i + 1)));
+        delay_queue_.AddTask(i * 200, 
+            std::bind(&DelayQueueSmallUnitTest::add, this, i, i + 1)));
   }
 
   // Wait for tasks to complete 
   for (int i = 0; i < num_tasks; i++) {
     EXPECT_EQ(task_future[i].get(), 2 * i + 1);
   }
+
+  // Check that the results are showing up in sequence in int_res_queue_
+  for (int i = 0; i < num_tasks; i++) {
+    int val;
+    EXPECT_TRUE(int_res_queue_.TryPop(val));
+    EXPECT_EQ(val, 2 * i + 1);
+  }
+  EXPECT_TRUE(int_res_queue_.Empty());
+}
+
+// Similar to the above test but with task added with a reverse order, i.e
+// the one that has longest delay time is inserted first
+TEST_F(DelayQueueSmallUnitTest, AddTaskInReverseSequence) {
+  int num_tasks(10);
+  std::vector<std::future<int>> task_future;
+  // Insert tasks with different delay
+  for (int i = 0; i < num_tasks; i++) {
+    task_future.emplace_back(
+        delay_queue_.AddTask((num_tasks - i) * 200, 
+            std::bind(&DelayQueueSmallUnitTest::add, this, i, i + 1)));
+  }
+
+  // Wait for tasks to complete 
+  for (int i = 0; i < num_tasks; i++) {
+    EXPECT_EQ(task_future[i].get(), 2 * i + 1);
+  }
+
+  // Check that the results are showing up in reverse sequence in the 
+  // int_res_queue_
+  for (int i = num_tasks-1; i >= 0; i--) {
+    int val;
+    EXPECT_TRUE(int_res_queue_.TryPop(val));
+    EXPECT_EQ(val, 2 * i + 1);
+  }
+  EXPECT_TRUE(int_res_queue_.Empty());
 }
