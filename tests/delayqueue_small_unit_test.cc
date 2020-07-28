dc8efa1b8e236b29b8cc8db64f46a74ff8999b41
Xi Cheng <cx19871012@gmail.com>
2020-07-28 02:15:03 +0000

    [test] Add multi-thread test cases to delayqueue_small_unit_test, have different threads to insert task and check results

--- tests/delayqueue_small_unit_test.cc
+++ tests/delayqueue_small_unit_test.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 #include <future>
 #include <iostream>
+#include <thread>
 #include <vector>
 
 #include "gtest/gtest.h"
@@ -25,7 +26,8 @@ class DelayQueueSmallUnitTest : public ::testing::Test {
 
 // Define several tasks with different delay time and makes sure that they all
 // execute with expected returns. In this specific case, we insert tasks 
-// in sequence with a delay time gradually increasing
+// in sequence with a delay time gradually increasing (with an interval of 
+// 200ms)
 TEST_F(DelayQueueSmallUnitTest, AddTaskInSequence) {
   int num_tasks(10);
   std::vector<std::future<int>> task_future;
@@ -53,7 +55,7 @@ TEST_F(DelayQueueSmallUnitTest, AddTaskInSequence) {
 // Similar to the above test but with task added with a reverse order, i.e
 // the one that has longest delay time is inserted first
 TEST_F(DelayQueueSmallUnitTest, AddTaskInReverseSequence) {
-  int num_tasks(10);
+  int num_tasks(100);
   std::vector<std::future<int>> task_future;
   // Insert tasks with different delay
   for (int i = 0; i < num_tasks; i++) {
@@ -69,10 +71,84 @@ TEST_F(DelayQueueSmallUnitTest, AddTaskInReverseSequence) {
 
   // Check that the results are showing up in reverse sequence in the 
   // int_res_queue_
-  for (int i = num_tasks-1; i >= 0; i--) {
+  for (int i = num_tasks - 1; i >= 0; i--) {
     int val;
     EXPECT_TRUE(int_res_queue_.TryPop(val));
     EXPECT_EQ(val, 2 * i + 1);
   }
   EXPECT_TRUE(int_res_queue_.Empty());
+}
+
+// Same scenario as to AddTaskInSequence but have different threads to submit
+// task separately
+TEST_F(DelayQueueSmallUnitTest, AddTaskInSequenceMultiThread) {
+  const int num_tasks(10);
+  std::vector<std::future<int>> task_future(num_tasks);
+  std::vector<std::thread> threads;
+  
+  // Insert each task by a separate thread
+  for (int i = 0; i < num_tasks; i++) {
+    threads.push_back(std::thread( [i, this, &task_future] () {
+      task_future[i] =  this->delay_queue_.AddTask(
+                            i * 200, 
+                            std::bind(&DelayQueueSmallUnitTest::add, 
+                                this, i, i + 1));
+    }));
+  }
+
+  // Wait for tasks to complete 
+  for (int i = 0; i < num_tasks; i++) {
+    EXPECT_EQ(task_future[i].get(), 2 * i + 1);
+  }
+
+  // Check that the results are showing up in sequence in int_res_queue_
+  for (int i = 0; i < num_tasks; i++) {
+    int val;
+    EXPECT_TRUE(int_res_queue_.TryPop(val));
+    EXPECT_EQ(val, 2 * i + 1);
+  }
+  EXPECT_TRUE(int_res_queue_.Empty());
+
+  // Join the threads
+  for (auto& t : threads) {
+    t.join();
+  }
+  threads.clear();
+}
+
+// Same scenario as to AddTaskInReverseSequence but have different threads 
+// to submit task separately
+TEST_F(DelayQueueSmallUnitTest, AddTaskInReverseSequenceMultiThread) {
+  const int num_tasks(100);
+  std::vector<std::future<int>> task_future(num_tasks);
+  std::vector<std::thread> threads;
+  
+  // Insert each task by a separate thread
+  for (int i = 0; i < num_tasks; i++) {
+    threads.push_back(std::thread( [i, this, &task_future] () {
+      task_future[i] =  this->delay_queue_.AddTask(
+                            (num_tasks - i) * 200, 
+                            std::bind(&DelayQueueSmallUnitTest::add, 
+                                this, i, i + 1));
+    }));
+  }
+
+  // Wait for tasks to complete 
+  for (int i = 0; i < num_tasks; i++) {
+    EXPECT_EQ(task_future[i].get(), 2 * i + 1);
+  }
+
+  // Check that the results are showing up in sequence in int_res_queue_
+  for (int i = num_tasks - 1; i >= 0; i--) {
+    int val;
+    EXPECT_TRUE(int_res_queue_.TryPop(val));
+    EXPECT_EQ(val, 2 * i + 1);
+  }
+  EXPECT_TRUE(int_res_queue_.Empty());
+
+  // Join the threads
+  for (auto& t : threads) {
+    t.join();
+  }
+  threads.clear();
 }
